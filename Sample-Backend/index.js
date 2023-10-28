5daed542f79e0ebfffe9f7913be63e888e163aa9
Allison Winkley <dev.allisonwinkley@gmail.com>
2023-10-28 11:41:09 +0800

    coded along with sample backend lesson

--- Sample-Backend/index.js
+++ Sample-Backend/index.js
@@ -1,50 +1,68 @@
-// // NODE.JS WAY WITHOUT FRAMEWORK
+/*
+// NODE.JS WAY WITHOUT FRAMEWORK
 
-// const http = require("http"); // common js way/version - can be converted into an ES6 module
-// // import http from "http"; - new way/version
+const http = require("http"); // common js way/version - can be converted into an ES6 module
+// import http from "http"; - new way/version
 
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     important: true,
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can only execute JavaScript",
-//     important: false,
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     important: true,
-//   },
-// ];
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can only execute JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
 
-// const app = http.createServer((request, response) => {
-//   // how to return an object
-//   // response.writeHead(200, { "Content-Type": "/plain" });
-//   // response.end("Hello, World!");
+const app = http.createServer((request, response) => {
+  // how to return an object
+  response.writeHead(200, { "Content-Type": "/plain" });
+  response.end("Hello, World!");
 
-//   // how to return JSON data
-//   // response.writeHead(200, { "Content-Type": "application/json" });
-//   // response.end(JSON.stringify({ message: "Hello, NodeJS!" }));
+  // how to return JSON data
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.end(JSON.stringify({ message: "Hello, NodeJS!" }));
 
-//   response.writeHead(200, { "Content-Type": "application/json" });
-//   response.end(JSON.stringify(notes));
-// });
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.end(JSON.stringify(notes));
+});
 
-// const PORT = 3001;
+const PORT = 3001;
 
-// app.listen(PORT);
-// console.log(`Server is running on port ${PORT}`);
+app.listen(PORT);
+console.log(`Server is running on port ${PORT}`);
+*/
 
 // EXPRESS is a NODE.js framework in the back-end side
 // EXPRESS (new, easier, and better way)
+
+/** RESTful API CONVENTION
+ 
+ * ROUTES           HTTP      ACTION                                            STATUS  
+ 
+ * api/notes/:id    GET       fetch a single resource                            200 OK
+ * api/notes        GET       fetch all resources                                200 OK
+ * api/notes        POST      create a new resource                              201 Created
+ * api/notes/:id    DELETE    delete a single resource                           204 No Content
+ * api/notes/:id    PUT       replace the specified resource
+ * api/notes/:id    PATCH     replaces a part of the specified resource
+ */
+
 import express from "express";
 
 const app = express();
 const PORT = 3001;
+
+// Express has a built-in middleware/method
+app.use(express.json()); // this is how we activate the json parser and needs to be placed on top so it can run first prior to the methods/requests
 
 let notes = [
   {
@@ -69,18 +87,18 @@ let notes = [
   },
 ];
 
-/** RESTful API CONVENTION
- * ROUTES           HTTP      ACTION
- * api/notes/:id    GET       fetch a single resource
- * api/notes        GET       fetch all resources
- * api/notes        POST      create a new resource
- * api/notes/:id    DELETE    delete a single resource
- * api/notes/:id    PUT       replace the specified resource
- * api/notes/:id    PATCH     replaces a part of the specified resource
- */
+function generateId() {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+
+  return maxId + 1;
+}
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello, Express!</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.status(200).json(notes);
 });
 
 app.get("/api/notes/:id", (request, response) => {
@@ -89,12 +107,22 @@ app.get("/api/notes/:id", (request, response) => {
   response.status(200).json(note);
 });
 
-app.get("/api/notes", (request, response) => {
-  response.status(200).json(notes);
-});
-
 app.post("/api/notes", (request, response) => {
-  response.send();
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ error: "content missing" });
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    important: body.important || false,
+  };
+
+  notes = notes.concat(note);
+
+  response.status(201).json(note);
 });
 
 app.delete("/api/notes/:id", (request, response) => {
